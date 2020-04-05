06f257952a64e2fd7dfe4f7a791ea49844ae2999
parang <appdler96@kaist.ac.kr>
2020-04-06 00:56:51 +0900

    build(route): Add react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import HotelListPage from './pages/HotelListPage';
 
 const App = () => {
   return (
-    <>
-      <HomePage />
-      <HotelListPage />
-    </>
+    <Switch>
+      <Route path="/web" exact component={HomePage} />
+      <Route path="/web/hotels" component={HotelListPage} />
+    </Switch>
   );
 }
 

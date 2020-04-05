6657ecbba4f5a87e9004f4b6708fba135188eb54
parang <appdler96@kaist.ac.kr>
2020-04-06 01:02:02 +0900

    feat(notFound): add NotFound component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import HotelListPage from './pages/HotelListPage';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
     <Switch>
       <Route path="/web" exact component={HomePage} />
       <Route path="/web/hotels" component={HotelListPage} />
+      <Route component={NotFound} />
     </Switch>
   );
 }

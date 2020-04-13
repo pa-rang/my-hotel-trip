7a2ce36a67cedad96dd4cabdfdddcd9e984fdfe5
parang <appdler96@kaist.ac.kr>
2020-04-13 16:31:53 +0900

    fix(hotelList): Fix lazy loading using hooks

--- src/components/HotelList/HotelList.js
+++ src/components/HotelList/HotelList.js
@@ -1,44 +1,29 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
-
-// const lazyRef = useRef(null);
-// const lazyObserver = new IntersectionObserver((entries, observer) => {
-//     if (entries[0].intersectionRatio > 0) {
-//         const target = entries[0].target;
-//         target.src = target.dataset.src;
-//         observer.unobserve(target);
-//     }
-// })
-// useEffect(() => {
-//     if (lazyRef.current) {
-//         lazyObserver.observe(lazyRef.current);
-//     }
-// }, [])
+import defaultImage from '../../icons/default-image.png';
 
 const HotelList = ({ id, name, freeServices, imageUrl, rate, reviewScore, totalReviewCount }) => {
 
-
-  // Lazy Loading
-  const images = document.querySelectorAll('.image');
+  // lazy Loading
+  const lazyLoadRef = useRef();
   const lazyLoadOption = { root: null, threshold: 0.5 };
-  const lazyLoadImage = (entries, observer) => {
-    entries.forEach(entry => {
-      const { target } = entry;
-      if (entry.isIntersecting) {
-        console.log("target", target);
-        target.src = target.dataset.src;
-        lazyLoadingIO.unobserve(target)
-      }
-    })
+  const lazyLoadhandler = (entries, observer) => {
+    const { target } = entries[0];
+    if (entries[0].isIntersecting) {
+      target.src = target.dataset.src;
+      lazyLoadObserver.unobserve(target);
+    }
   };
-  const lazyLoadingIO = new IntersectionObserver(lazyLoadImage, lazyLoadOption);
-  images.forEach(image => lazyLoadingIO.observe(image));
+  const lazyLoadObserver = new IntersectionObserver(lazyLoadhandler, lazyLoadOption);
+  useEffect(() => {
+    lazyLoadObserver.observe(lazyLoadRef.current);
+  }, [])
 
   return (
     <div style={{ border: "1px solid #363636", margin: "24px", width: "480px" }}>
       <div>{name}</div>
       <div>{freeServices}</div>
-      <img className="image" data-src={imageUrl} alt={name} title={name} style={{ width: "200px", backgroundColor: "f8f8f8" }}/>
+      <img ref={lazyLoadRef} src={defaultImage} data-src={imageUrl} alt={name} title={name} style={{ width: "200px", height: "200px" }} />
       <div>{rate}</div>
       <div>{reviewScore}</div>
       <div>{totalReviewCount}</div>

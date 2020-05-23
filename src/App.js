207407a759566092e90c058a12250e9d9fccc113
parang <appdler96@kaist.ac.kr>
2020-05-23 17:05:01 +0900

    temp: optimization test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import ErrorBoundary from 'ErrorBoundary';
 
-import HomePage from './pages/HomePage';
-import HotelListPage from './pages/HotelListPage';
-import NotFound from './pages/NotFound';
+const HomePage = React.lazy(() => import('pages/HomePage'));
+const HotelListPage = React.lazy(() => import('pages/HotelListPage'));
+const NotFound = React.lazy(() => import('pages/NotFound'));
 
-const App = () => {
-  return (
-    <Switch>
-      <Route path="/" exact component={HomePage} />
-      <Route path="/hotels" component={HotelListPage} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
+const App = () => (
+  <ErrorBoundary>
+    <Suspense fallback={<h1 style={{ color: 'red' }}>Loading...</h1>}>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/hotels" component={HotelListPage} />
+        <Route component={NotFound} />
+      </Switch>
+    </Suspense>
+  </ErrorBoundary>
+);
 
 export default App;
--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React from 'react'
+import Input from './Input'
 
-const NotFound = () => {
-  return(
-    <div>
-      <h1>Not Found</h1>
-    </div>
-  );
-};
+class NotFound extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  }
+  
+  render() {
+    const { email, password } = this.state
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <Input
+          type="email"
+          placeholder="이메일"
+          value={email}
+          onChange={this.handleChangeEmail}
+        />
+        <Input
+          type="password"
+          placeholder="비밀번호"
+          password={password}
+          onChange={this.handleChangePassword}
+        />
+        <button type="submit">가입하기</button>
+        <button type="button" onClick={this.handleReset}>초기화</button>
+      </form>
+    );
+  }
 
-export default NotFound;
+  handleChangeEmail = ({ target: { value } }) => {
+    this.setState({ email: value })
+  }
+  
+  handleChangePassword = ({ target: { value } }) => {
+    this.setState({ password: value })
+  }
+
+  handleSubmit = () => {
+    console.log(this.state.email, this.state.password)
+  }
+
+  handleReset = () => {
+    this.setState({
+      email: '',
+      password: '',
+    })
+  }
+}
+
+export default NotFound
